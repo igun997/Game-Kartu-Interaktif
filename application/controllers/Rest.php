722517651ad7e27857e3e2a299d8fdf48f443671
Indra Gunanda <indra.gunanda@gmail.com>
2017-11-18 12:40:24 +0700

    Add Generate Card Add Resize to 500x714 Dimension Image After Upload
    Task #1 - CRUD Modul Tokoh

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -49,6 +49,79 @@ class Rest extends REST_Controller {
         }
     }
 
+    function _resize_image($file, $w, $h, $crop = FALSE) {
+        list($width, $height) = getimagesize($file);
+        $r = $width / $height;
+        if ($crop) {
+            if ($width > $height) {
+                $width = ceil($width - ($width * abs($r - $w / $h)));
+            } else {
+                $height = ceil($height - ($height * abs($r - $w / $h)));
+            }
+            $newwidth = $w;
+            $newheight = $h;
+        } else {
+            if ($w / $h > $r) {
+                $newwidth = $h * $r;
+                $newheight = $h;
+            } else {
+                $newheight = $w / $r;
+                $newwidth = $w;
+            }
+        }
+        $src = imagecreatefromjpeg($file);
+        $dst = imagecreatetruecolor($newwidth, $newheight);
+        imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+        return $dst;
+    }
+
+    function generatecard_get($card = '') {
+        $this->_restrict(1);
+        if ($card != '') {
+            $tokoh = new Entities\Tokoh();
+            $tokoh->find($card);
+            if ($tokoh->count_rows() > 0) {
+                $this->load->library('ciqrcode');
+                $config['cacheable'] = true; //boolean, the default is true
+                $config['cachedir'] = ''; //string, the default is application/cache/
+                $config['errorlog'] = ''; //string, the default is application/logs/
+                $config['quality'] = true; //boolean, the default is true
+
+                $config['black'] = array(224, 255, 255); // array, default is array(255,255,255)
+                $config['white'] = array(224, 255, 255); // array, default is array(0,0,0)
+                $this->ciqrcode->initialize($config);
+                $params['data'] = $tokoh->getId_data();
+                ob_start();
+                $this->ciqrcode->generate($params);
+                $imgString = ob_get_clean();
+                $cardTemplate = base_url("_upload/card/cardTemplate.jpg");
+                $cardNow = base_url("_upload/foto/" . $tokoh->getFoto());
+                $dest = imagecreatefromjpeg($cardTemplate);
+                $qr = imagecreatefromstring($imgString);
+                $textcolor = imagecolorallocate($dest, 0, 0, 0);
+                $font = "assets/OpenSans-Regular.ttf";
+                $fontSize = "18";
+                ob_start();
+                imagettftext($dest, 25, 90, 570, 750, $textcolor, $font, "AA");
+                imagettftext($dest, 25, 90, 1268, 1620, $textcolor, $font, "AA");
+                $foto = $this->_resize_image($cardNow, 350, 340);
+                imagecopymerge($dest, $foto, 70, 64, 0, 0, 238, 340, 100);
+                imagecopymerge($dest, $qr, 140, 450, 0, 0, 100, 100, 100);
+                imagejpeg($dest);
+                imagedestroy($dest);
+                //header('Content-Type: image/jpeg');
+                $realImage = base64_encode(ob_get_contents());
+                ob_clean();
+                $this->response(array("status" => 1, "msg" => "Success Generated", "data" => "data:image/jpeg;base64," . $realImage));
+            } else {
+                $this->response(array("status" => 0, "msg" => "Card Not Found"));
+            }
+        } else {
+            $this->response(array("status" => 0, "msg" => "No Card ID"));
+        }
+    }
+
     function getDataTokoh_post() {
         $this->_restrict(1);
         $this->load->library("datatables");
@@ -85,24 +158,35 @@ class Rest extends REST_Controller {
         if (isset($_FILES["foto"]["tmp_name"])) {
             $config['upload_path'] = '_upload/foto/';
             @unlink($config['upload_path'] . $fotoNow);
-            $config['allowed_types'] = 'jpg|gif|png|jpeg';
+            $config['allowed_types'] = 'jpg|jpeg';
             $config['max_size'] = 500;
             $this->load->library('upload', $config, 'fotoObjek');
             $this->fotoObjek->initialize($config);
             $uploadFoto = $this->fotoObjek->do_upload('foto');
             if ($uploadFoto) {
                 $dataFoto = $this->fotoObjek->data();
+                $this->load->library('image_lib');
+                $configer = array(
+                    'image_library' => 'gd2',
+                    'source_image' => $dataFoto['full_path'],
+                    'maintain_ratio' => false,
+                    'width' => 500,
+                    'height' => 714,
+                );
+                $this->image_lib->clear();
+                $this->image_lib->initialize($configer);
+                $this->image_lib->resize();
             } else {
                 $dataFoto = $this->fotoObjek->display_errors();
             }
-        }else{
+        } else {
             $noFoto = true;
         }
         if (isset($_FILES["video"]["tmp_name"])) {
             $config['upload_path'] = '_upload/video/';
             @unlink($config['upload_path'] . $videoNow);
             $config['allowed_types'] = 'mp4|flv';
-            $config['max_size'] = 5000;
+            $config['max_size'] = 50000;
             $this->load->library('upload', $config, 'videoObjek');
             $this->videoObjek->initialize($config);
             $uploadVideo = $this->videoObjek->do_upload('video');
@@ -111,19 +195,18 @@ class Rest extends REST_Controller {
             } else {
                 $dataVideo = $this->videoObjek->display_errors();
             }
-        }else{
+        } else {
             $noVideo = true;
         }
-        if($noFoto && $noVideo)
-        {
-            $this->response(array("status" => 1, "dataFoto" => (isset($dataFoto["file_name"]))?$dataFoto["file_name"]:null, "dataVideo" => (isset($dataVideo["file_name"]))?$dataVideo["file_name"]:null));
+        if ($noFoto && $noVideo) {
+            $this->response(array("status" => 1, "curFoto" => $fotoNow, "vidNow" => $videoNow, "dataFoto" => (isset($dataFoto["file_name"])) ? $dataFoto["file_name"] : null, "dataVideo" => (isset($dataVideo["file_name"])) ? $dataVideo["file_name"] : null));
             exit();
         }
-        if ($uploadFoto && $uploadVideo) {
-            $this->response(array("status" => 1, "dataFoto" => (isset($dataFoto["file_name"]))?$dataFoto["file_name"]:null, "dataVideo" => (isset($dataVideo["file_name"]))?$dataVideo["file_name"]:null));
+        if ($uploadFoto || $uploadVideo) {
+            $this->response(array("status" => 1, "curFoto" => $fotoNow, "vidNow" => $videoNow, "dataFoto" => (isset($dataFoto["file_name"])) ? $dataFoto["file_name"] : null, "dataVideo" => (isset($dataVideo["file_name"])) ? $dataVideo["file_name"] : null));
         } else {
             //$this->response($_FILES["foto"]);
-            $response = array("status" => 0, "msg" => "Fail to Upload", "debug" => array("foto" => array("status" => $uploadFoto, "data" => $dataFoto, "files" => $_FILES["foto"]), "video" => array("status" => $uploadVideo, "data" => $dataVideo, "files" => $_FILES["video"])));
+            $response = array("status" => 0, "msg" => "Fail to Upload", "debug" => array("foto" => array("status" => $uploadFoto, "data" => $dataFoto, "files" => null), "video" => array("status" => $uploadVideo, "data" => $dataVideo, "files" => null)));
             if ($response["debug"]["foto"]["status"]) {
                 unlink("_upload/foto/" . $response["debug"]["foto"]["data"]["file_name"]);
             }
@@ -151,21 +234,23 @@ class Rest extends REST_Controller {
         $tokoh->setTanggal_lahir($data->post("tanggal_lahir", true));
         $tokoh->setTanggal_wafat($data->post("tanggal_wafat", true));
         $tokoh->setTentang($data->post("txtArea", false));
-        if($data->post("foto", true) != null)
-        {
+        if ($data->post("foto", true) != null) {
             $tokoh->setFoto($data->post("foto", true));
+        } else {
+            $tokoh->setFoto($data->post("curFoto", true));
         }
-        if($data->post("video", true) != null)
-        {
+        if ($data->post("video", true) != null) {
             $tokoh->setVideo($data->post("video", true));
+        } else {
+            $tokoh->setFoto($data->post("vidNow", true));
         }
-        
+
         $tokoh->setId_users($this->session->id_users);
         $status = $tokoh->update();
         if ($status) {
             $d = array("status" => 1, "msg" => "Success Update");
         } else {
-            $d = array("status" => 0, "msg" => "Fail To Update","debug"=>$data->post("id_data", true));
+            $d = array("status" => 0, "msg" => "Fail To Update", "debug" => $data->post("vidNow", true));
         }
         $this->response($d);
     }
@@ -202,13 +287,24 @@ class Rest extends REST_Controller {
         $config["mod_mime_fix"] = true;
         if (isset($_FILES["foto"]["tmp_name"])) {
             $config['upload_path'] = '_upload/foto/';
-            $config['allowed_types'] = 'jpg|gif|png|jpeg';
+            $config['allowed_types'] = 'jpg|jpeg';
             $config['max_size'] = 500;
             $this->load->library('upload', $config, 'fotoObjek');
             $this->fotoObjek->initialize($config);
             $uploadFoto = $this->fotoObjek->do_upload('foto');
             if ($uploadFoto) {
                 $dataFoto = $this->fotoObjek->data();
+                $this->load->library('image_lib');
+                $configer = array(
+                    'image_library' => 'gd2',
+                    'source_image' => $dataFoto['full_path'],
+                    'maintain_ratio' => false,
+                    'width' => 500,
+                    'height' => 714,
+                );
+                $this->image_lib->clear();
+                $this->image_lib->initialize($configer);
+                $this->image_lib->resize();
             } else {
                 $dataFoto = $this->fotoObjek->display_errors();
             }
@@ -216,7 +312,7 @@ class Rest extends REST_Controller {
         if (isset($_FILES["video"]["tmp_name"])) {
             $config['upload_path'] = '_upload/video/';
             $config['allowed_types'] = 'mp4|flv';
-            $config['max_size'] = 5000;
+            $config['max_size'] = 50000;
             $this->load->library('upload', $config, 'videoObjek');
             $this->videoObjek->initialize($config);
             $uploadVideo = $this->videoObjek->do_upload('video');
@@ -250,7 +346,7 @@ class Rest extends REST_Controller {
         if ($res) {
             $assetVideo = "_upload/video/";
             $assetFoto = "_upload/foto/";
-            if (unlink($assetVideo . $nameVideo) && unlink($assetFoto . $nameFoto)) {
+            if (@unlink($assetVideo . $nameVideo) && @unlink($assetFoto . $nameFoto)) {
                 $this->response(array("status" => 1, "msg" => "Tokoh Di Hapus"));
             } else {
                 $this->response(array("status" => 0, "msg" => "Assets Tokoh Gagal di Hapus"));
@@ -262,6 +358,9 @@ class Rest extends REST_Controller {
 
     function test_get() {
         $this->_restrict(1);
+        $tokoh = new Entities\Tokoh();
+        $tokoh->find();
+        $this->response($tokoh->fetchAll());
     }
 
     function logout_get() {
